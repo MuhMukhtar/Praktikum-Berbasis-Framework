a027ff49e2de41313844654edd00220692286b75
Muhammad Mukhtar <mukhtar55217@gmail.com>
2022-03-16 10:52:58 +0800

    First - Nesting Router

--- Modul 5 - React Router/react-router-dom/src/App.js
+++ Modul 5 - React Router/react-router-dom/src/App.js
@@ -1,25 +1,168 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// export default function BasicExample(){
+//   return(
+//     <Router>
+//       <div>
+//         <ul>
+//         <il>
+//             <Link to="/">Home</Link>
+//           </il>
+//           <il>
+//             <Link to="/about">About</Link>
+//           </il>
+//           <il>
+//             <Link to="/dashboard">Dashboard</Link>
+//           </il>
+//         </ul>
+//         <hr/>
+
+//         <Switch>
+//           <Route exact patch="/">
+//             <Home />
+//           </Route>
+//           <Route exact patch="/about">
+//             <About />
+//           </Route>
+//           <Route exact patch="/dashboard">
+//             <Dashboard />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+function Home() {
+  return(
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+// function About() {
+//   return(
+//     <div>
+//       <h2>About</h2>
+//     </div>
+//   );
+// }
+
+// function Dashboard() {
+//   return(
+//     <div>
+//       <h2>Dashboard</h2>
+//     </div>
+//   );
+// }
+
+// export default function ParamsExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <h2>Accounts</h2>
+//         <ul>
+//           <li>
+//             <Link to="/netflix">Netflix</Link>
+//           </li>
+//           <li>
+//             <Link to="/gmail">Gmail</Link>
+//           </li>
+//           <li>
+//             <Link to="/yahoo">Yahoo</Link>
+//           </li>
+//           <li>
+//             <Link to="/amazon">Amazon</Link>
+//           </li>
+//         </ul>
+        
+//         <Switch>
+//           <Route path="/:id" children={<Child/>}/>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function Child() {
+//   let { id } = useParams();
+
+//   return (
+//     <div>
+//       <h3>ID: {id}</h3>
+//     </div>
+//   );
+// }
+
+export default function NestingExample(){
+  return(
+    <Router>
+      <div>
+        <ul>
+        <il>
+            <Link to = "/">Home</Link>
+          </il>
+          <il>
+            <Link to = "/topics">Topics</Link>
+          </il>
+        </ul>
+        <hr/>
+
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/topics">
+            <Topics />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Topics() {
+  let {path, url} = useRouteMatch();
+  return(
+    <div>
+      <h2>Topics</h2>
+      <ul>
+      <li>
+          <Link to={`${url}/Sate, Nasi Goreng`}>Kuliner</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Wisata Alam, Museum`}>Travelling</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Ibis, JW Marriot`}>Review Hotel</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route exact path={`${path}/:tipicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let {topicId} = useParams();
+
+  return(
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
