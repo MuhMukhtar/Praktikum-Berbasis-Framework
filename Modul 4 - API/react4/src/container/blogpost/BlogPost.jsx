69519562d264e7cbf72b286d4d4744ed234eecd0
Muhammad Mukhtar <mukhtar55217@gmail.com>
2022-03-16 10:01:58 +0800

    Praktikum 3

--- Modul 4 - API/react4/src/container/blogpost/BlogPost.jsx
+++ Modul 4 - API/react4/src/container/blogpost/BlogPost.jsx
@@ -18,7 +18,7 @@ class BlogPost extends Component{
         listArtikel: []
     }
 
-    componentDidMount(){
+    ambilDataDariServerAPI = () => {
         fetch('http://localhost:3001/posts')
             .then(response => response.json())
             .then(jsonHasilAmbilDariAPI => {
@@ -28,13 +28,24 @@ class BlogPost extends Component{
             })
     }
 
+    componentDidMount(){
+        this.ambilDataDariServerAPI()
+    }
+
+    handleHapusArtikel = (data) => {
+        fetch(`http://localhost:3001/posts/${data}`, {method: 'DELETE'})
+            .then(res => {
+                this.ambilDataDariServerAPI()
+            })
+    }
+
     render() {
         return(
             <div className="post-artikel">
                 <h2>Daftar Artikel</h2>
                 {
                     this.state.listArtikel.map(artikel => {
-                        return <Post key = {artikel.id} judul = {artikel.title} isi = {artikel.body}/>
+                        return <Post key = {artikel.id} judul = {artikel.title} isi = {artikel.body} hapusArtikel = {this.handleHapusArtikel}/>
                     })
                 }
             </div>
