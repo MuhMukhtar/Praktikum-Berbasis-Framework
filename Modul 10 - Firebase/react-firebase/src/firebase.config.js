c274eca6dfab56b2eff8c9da0a9ea483fd36ffde
Muhammad Mukhtar <mukhtar55217@gmail.com>
2022-05-23 18:52:08 +0800

    Modul 10

---
+++ Modul 10 - Firebase/react-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAC4LTp6Yi9HDLDw3mbGcApmbaUbWDFj4s",
+  authDomain: "react-firebase-7cfda.firebaseapp.com",
+  projectId: "react-firebase-7cfda",
+  storageBucket: "react-firebase-7cfda.appspot.com",
+  messagingSenderId: "797645386997",
+  appId: "1:797645386997:web:9322e951f0c3a004b841bc",
+  measurementId: "G-XK7T0LWTF4"
+}; 
