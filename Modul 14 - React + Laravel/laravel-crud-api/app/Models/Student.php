a20fe88f00f73afd77e2e770c7fe019a71bbab00
Muhammad Mukhtar <mukhtar55217@gmail.com>
2022-06-08 12:12:37 +0800

    Modul 14 Finish part 2

--- Modul 14 - React + Laravel/laravel-crud-api/app/Models/Student.php
+++ Modul 14 - React + Laravel/laravel-crud-api/app/Models/Student.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "first_name", "last_name", "full_name", "email", "phone"
+    ];
 }
--- Modul 14 - React + Laravel/laravel-crud-api/routes/api.php
+++ Modul 14 - React + Laravel/laravel-crud-api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post("create-student","App\Http\Controllers\StudentController@createStudent");
+
+Route::get("students", "App\Http\Controllers\StudentController@studentsListing");
+
+Route::get("student/{id}", "App\Http\Controllers\StudentController@studentDetail");
+
+Route::delete("student/{id}", "App\Http\Controllers\StudentController@studentDelete");
