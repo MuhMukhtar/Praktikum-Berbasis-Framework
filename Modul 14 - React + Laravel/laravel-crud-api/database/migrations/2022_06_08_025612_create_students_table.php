e3b01690d63ba07858cac363f88bd98452c67e5c
Muhammad Mukhtar <mukhtar55217@gmail.com>
2022-06-08 11:20:36 +0800

    migrate

--- Modul 14 - React + Laravel/laravel-crud-api/database/migrations/2022_06_08_025612_create_students_table.php
+++ Modul 14 - React + Laravel/laravel-crud-api/database/migrations/2022_06_08_025612_create_students_table.php
@@ -15,8 +15,13 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string("first_name")->nullable();
+            $table->string("last_name")->nullable();
+            $table->string("full_name")->nullable();
+            $table->string("email")->nullable();
+            $table->string("phone")->nullable();
             $table->timestamps();
-        });
+            });
     }
 
     /**
