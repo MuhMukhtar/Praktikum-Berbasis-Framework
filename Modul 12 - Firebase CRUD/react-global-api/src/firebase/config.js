6d8f250f0420a0f04192cefa24fcc7e1a2e2f77b
Muhammad Mukhtar <mukhtar55217@gmail.com>
2022-06-08 09:45:13 +0800

    modul 12

---
+++ Modul 12 - Firebase CRUD/react-global-api/src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBsiP6kHy4YT10noC_127nqoryS5a2R6q0",
+  authDomain: "fir-modul12.firebaseapp.com",
+  databaseURL: "https://fir-modul12-default-rtdb.firebaseio.com",
+  projectId: "fir-modul12",
+  storageBucket: "fir-modul12.appspot.com",
+  messagingSenderId: "380288319896",
+  appId: "1:380288319896:web:113b52e8be90bd10356bed",
+  measurementId: "G-D0SNMT7MMF"
+};
+
+  export default firebaseConfig;
